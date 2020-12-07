49ce64dc7935021fcd6d3d64a125c7a8959abb55
jbji <jbji@foxmail.com>
2020-12-07 20:21:35 +0800

    补充

---
+++ 2020-ds-wordCompress/cmake-build-debug/CMakeFiles/2020_ds_wordCompress.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "2020_ds_wordCompress.exe"
+  "2020_ds_wordCompress.exe.manifest"
+  "2020_ds_wordCompress.pdb"
+  "CMakeFiles/2020_ds_wordCompress.dir/main.cpp.obj"
+  "lib2020_ds_wordCompress.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2020_ds_wordCompress.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
